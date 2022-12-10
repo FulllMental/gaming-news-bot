36ddff10298dae643b633cb564cd545f22c12a50
FulllMental <104234625+FulllMental@users.noreply.github.com>
2022-12-10 18:42:54 +0300

    Add blacklist check for article titles

--- main.py
+++ main.py
@@ -1,27 +1,65 @@
-import datetime
+import pathlib
 import time
+from datetime import datetime
+from os.path import splitext
+from urllib.parse import unquote, urlsplit
 
+import requests
+from bs4 import BeautifulSoup as BS
 from environs import Env
 
-from shazoo_parse import get_all_links, download_shazoo_picture
+from shazoo_parse import get_all_links
 from telegram_bot import bot_message
+
+
+def get_article_text(article_link):
+    response = requests.get(article_link)
+    response.raise_for_status()
+
+    article = BS(response.content, 'html.parser')
+    results = article.find_all('p')
+    article = ''
+    for i in results:
+        article += f'{i.text}\n'
+    return article
+
+
+def download_shazoo_picture(directory, shazoo_picture_url):
+    extension = get_picture_extension(shazoo_picture_url)
+    pathlib.Path(f'{directory}').mkdir(parents=True, exist_ok=True)
+    filename = f'{directory}/hubble.{extension}'
+
+    response = requests.get(shazoo_picture_url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+def get_picture_extension(shazoo_picture_url):
+    link_split = urlsplit(shazoo_picture_url)
+    file_name = unquote(link_split[2])
+    file_extension = splitext(file_name)
+    return file_extension[1]
+
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
-    pause_time = env('PAUSE_TIME')
+    pause_time = env.int('PAUSE_TIME')
     last_check_date = env('LAST_CHECK_DATE')
     shazoo_url = env('SHAZOO_URL')
+    blacklist = env.list('BLACKLIST')
     directory = 'shazoo_images'
 
     while True:
-        shazoo_news = get_all_links(shazoo_url, last_check_date)
+        shazoo_news = get_all_links(shazoo_url, last_check_date, blacklist)
         for post in shazoo_news:
-            shazoo_picture_url = post['picture_link']
             bot_message(article_link=post['article_link'], article_title=post['article_title'])
-            download_shazoo_picture(directory, shazoo_picture_url)
+            # shazoo_picture_url = post['picture_link']
+            # download_shazoo_picture(directory, shazoo_picture_url)
 
-        last_check_date = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M")
+        last_check_date = datetime.utcnow().strftime("%Y-%m-%dT%H:%M")
         print(f'Пост отправлен, следующий пост будет только тот, что запостили после {last_check_date}')
-        time.sleep(int(pause_time))
+        time.sleep(pause_time)
 
--- shazoo_parse.py
+++ shazoo_parse.py
@@ -1,60 +1,34 @@
-import pathlib
-from os.path import splitext
-from urllib.parse import unquote, urlsplit
-
 import requests
 from bs4 import BeautifulSoup as BS
 
 
-def get_all_links(shazoo_url, last_check_date):
+def get_all_links(shazoo_url, last_check_date, blacklist):
     response = requests.get(shazoo_url)
     response.raise_for_status()
 
-    news = []
+    shazoo_news = []
     html = BS(response.content, 'html.parser')
     posts = html.find_all('div', class_="flex flex-col gap-2 py-6 first:pt-0")
     for post in posts:
         post_date = post.time.attrs['datetime'][:-11]
         if last_check_date >= post_date:
-            return news
+            return shazoo_news
         ppt = post.find('div', class_='flex-shrink-0')
         article_title = ppt.a['title']
-        article_link = ppt.a['href']
-
-        news.append({'article_title': article_title,
-                     'picture_link': ppt.img['src'],
-                     'article_link': article_link})
-                     # 'article_text': get_article_text(article_link)})
-    return news
-
-
-def get_article_text(article_link):
-    response = requests.get(article_link)
-    response.raise_for_status()
-
-    article = BS(response.content, 'html.parser')
-    results = article.find_all('p')
-    article = ''
-    for i in results:
-        article += f'{i.text}\n'
-    return article
+        if check_blacklist(article_title, blacklist):
+            continue
+        shazoo_news.append(
+            {
+                'article_title': article_title,
+                'picture_link': ppt.img['src'],
+                'article_link': ppt.a['href']
+            }
+        )
+    return shazoo_news
 
 
-def download_shazoo_picture(directory, shazoo_picture_url):
-    extension = get_extension(shazoo_picture_url)
-    pathlib.Path(f'{directory}').mkdir(parents=True, exist_ok=True)
-    filename = f'{directory}/hubble.{extension}'
-
-    response = requests.get(shazoo_picture_url)
-    response.raise_for_status()
-
-    with open(filename, 'wb') as file:
-        file.write(response.content)
-
-
-def get_extension(shazoo_picture_url):
-    link_split = urlsplit(shazoo_picture_url)
-    file_name = unquote(link_split[2])
-    file_extension = splitext(file_name)
-    return file_extension[1]
-
+def check_blacklist(article_title, blacklist):
+    for banword in blacklist:
+        if banword in article_title.lower():
+            return True
+    return False
